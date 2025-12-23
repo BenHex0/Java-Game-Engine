2071e6e718d7ec7d3e81af9c60ac8aa486f040aa
BenHex0 <email@.com>
2025-12-23 02:15:03 +0200

    Modify Engine.java

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -10,6 +10,8 @@ import game.ui.EndScreen;
 import game.ui.LevelFinished;
 import game.ui.MainMenu;
 import game.ui.ScoreUI;
+import main.Main;
+
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import javax.swing.JFrame;
@@ -32,20 +34,20 @@ public class Engine extends Canvas implements Runnable {
 
     // UI
     private static UI currentUI;
+    public static int mainMenuScreen = 1;
+    public static int scoreScreen = 2;
+    public static int deathScreen = 3;
+    public static int winScreen = 4;
+    public static int endScreen = 5;
+    
+    // Levels
+    public static Level currentLevel;
     public static int level1 = 1;
     public static int level2 = 2;
     public static int level3 = 3;
     public static int level4 = 4;
     public static int level5 = 5;
     public static int level6 = 6;
-
-    // Levels
-    public static Level currentLevel;
-    public static int mainMenuScreen = 1;
-    public static int scoreScreen = 2;
-    public static int deathScreen = 3;
-    public static int winScreen = 4;
-    public static int endScreen = 5;
 
     // GAME STATES
     public static int current_state = 0;
@@ -65,31 +67,31 @@ public class Engine extends Canvas implements Runnable {
     }
 
     public static void setCurrentLevel(int level) {
-        if (level == 1) {
-            currentLevel = new Level01("assets/world/Map01-Forest.png", inputHandler);
-        } else if (level == 2) {
-            currentLevel = new Level02("assets/world/Map02-Dun.png", inputHandler);
-        } else if (level == 3) {
-            currentLevel = new Level03("assets/world/Map03-Dun.png", inputHandler);
-        } else if (level == 4) {
-            currentLevel = new Level04("assets/world/Map04-island.png", inputHandler);
-        } else if (level == 5) {
-            currentLevel = new Level05("assets/world/Map05-island.png", inputHandler);
-        } else if (level == 6) {
-            currentLevel = new Level06("assets/world/Map06-island.png", inputHandler);
+        if (level == level1) {
+            currentLevel = new Level01("/world/Map01-Forest.png", inputHandler);
+        } else if (level == level2) {
+            currentLevel = new Level02("/world/Map02-Dun.png", inputHandler);
+        } else if (level == level3) {
+            currentLevel = new Level03("/world/Map03-Dun.png", inputHandler);
+        } else if (level == level4) {
+            currentLevel = new Level04("/world/Map04-island.png", inputHandler);
+        } else if (level == level5) {
+            currentLevel = new Level05("/world/Map05-island.png", inputHandler);
+        } else if (level == level6) {
+            currentLevel = new Level06("/world/Map06-island.png", inputHandler);
         }
     }
 
     public static void setCurrentUI(int ui) {
-        if (ui == 1) {
+        if (ui == mainMenuScreen) {
             currentUI = new MainMenu(screenWidth * scale, screenHeight * scale, inputHandler);
-        } else if (ui == 2) {
+        } else if (ui == scoreScreen) {
             currentUI = new ScoreUI(screenWidth * scale, screenHeight * scale, inputHandler);
-        } else if (ui == 3) {
+        } else if (ui == deathScreen) {
             currentUI = new DeathScreen(screenWidth * scale, screenHeight * scale, inputHandler);
-        } else if (ui == 4) {
+        } else if (ui == winScreen) {
             currentUI = new LevelFinished(screenWidth * scale, screenHeight * scale, inputHandler);
-        } else if (ui == 5) {
+        } else if (ui == endScreen) {
             currentUI = new EndScreen(screenWidth * scale, screenHeight * scale, inputHandler);
         }
     }
@@ -187,6 +189,8 @@ public class Engine extends Canvas implements Runnable {
     }
 
     public void startGame() {
+        Image icon = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/icon.png"));
+        window.setIconImage(icon);
         window.add(this);
         window.pack();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
